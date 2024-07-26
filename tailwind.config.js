fcebcde54fb41933b1b0aa9fe50eb67c4fc6d7f8
Kölker <jkoelker@iq-agrar.de>
2024-07-26 11:52:46 +0200

    added stwui libary with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+		content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/stwui/**/*.{svelte,js,ts,html}',
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('stwui/plugin'),
+	]
+};
